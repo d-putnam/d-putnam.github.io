e1d90af6b4290d9923d3acd8c765ab9aa1bca32e
d-putnam <dan@dputnam.net>
2021-01-02 17:35:57 -0500

    cleaned up

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ let packRGBA = function(r, g, b, a=0.25) {
 // On page load, get three random RGB triples
 // Use them to set the background and link colors
 let colors = [];
-let open = function() {
+let open = () => {
     let rgbArr = [];
     for (let i = 0; i < 3; i++) {
         let rgb = {r: 0, g: 0, b: 0, target: "stop[n]"};
@@ -26,7 +26,7 @@ let open = function() {
         // Pack the rgb values into a string  0.15 OPACITY
         colors[i] = packRGBA(rgbArr[i].r, rgbArr[i].g, rgbArr[i].b, 1);
         // Update each gradient stop
-        $(rgbArr[i].target).attr("stop-color", colors[i]);
+        document.querySelector(rgbArr[i].target).style.stopColor = colors[i]
     }
     // Get a fourth (darker) color
     let color = {r: 0, g: 0, b: 0};
@@ -36,12 +36,18 @@ let open = function() {
     rgbArr[3] = color;
     // Set thumbnail to dark color 0.15 OPACITY
     colors[3] = packRGBA(rgbArr[3].r, rgbArr[3].g, rgbArr[3].b,0.15);
-    $(".thumb").css("background-color", colors[3]);
+    document.querySelectorAll('.thumb').forEach(thumb => {
+        thumb.style.backgroundColor = colors[3]
+    })
     // Set overlay to dark color FULL OPACITY
     colors[4] = packRGBA(rgbArr[3].r, rgbArr[3].g, rgbArr[3].b,1);
-    $(".overlay").css("background-color", colors[4]);
+    document.querySelectorAll('.overlay').forEach(thumb => {
+        thumb.style.backgroundColor = colors[4]
+    })
     // Reveal the lables now to mitigate flash 
-    $(".label").css("background-color", "white");     
+    document.querySelectorAll('.label').forEach(thumb => {
+        thumb.style.backgroundColor = 'white'
+    })
 
     // Get a fifth (lightest) color
     let lcolor = {r: 0, g: 0, b: 0};
@@ -51,8 +57,12 @@ let open = function() {
     rgbArr[4] = lcolor;
     // Set links to light color FULL OPACITY
     colors[5] = packRGBA(rgbArr[4].r, rgbArr[4].g, rgbArr[4].b,1);
-    $(".nav-link").css("color", colors[5]);
-    $(".content a").css("color", colors[5]);
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.style.color = colors[5]
+    })
+    document.querySelectorAll('.content a').forEach(link => {
+        link.style.color = colors[5]
+    })
 
     // Get colors for logo hover - colors[6-10]
     for (let i=0; i<5; i++){
@@ -61,20 +71,46 @@ let open = function() {
     }
 };
 
-$(document).ready(open());
+open();
 
 
 
-// Resize function for about page
-function reSize(x) {
-    if (x.matches) { // If media query matches
-        $('#right').css("display", "none");
-        $('#left').removeClass("col-7");
-        $('#smpic').removeClass("hidden");
+// Reload link gets new colors without reloading page
+document.querySelectorAll('.reload').forEach(link => {
+    link.onclick = () => {
+        open()
+        return false
     }
-    else {
-        $('#right').css("display", "block");
-        $('#left').addClass("col-7");
-        $('#smpic').addClass("hidden");
+})
+
+// On link hover, color 5=>4
+document.querySelectorAll('a').forEach(link => {
+    link.onmouseover = () => {
+        link.style.color = colors[4]
     }
-}
+    link.onmouseleave = () => {
+        link.style.color = colors[5]
+    }
+})
+
+// On logo hover, cycle through colors 6-11
+document.querySelectorAll('.logo').forEach(logo => {
+    let hover
+    logo.onmouseover = async () => {
+        hover = true
+        let i = 0
+        while (hover) {
+            logo.style.color = colors[i+6]
+            i++
+            if (i === 5) {
+                i = 0
+            }
+            await sleep(100)
+        }
+        logo.style.color = '#252525'
+    } 
+
+    logo.onmouseleave = () => {
+        hover = false
+    }
+})
