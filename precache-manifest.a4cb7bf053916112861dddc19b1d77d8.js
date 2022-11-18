7c79c71657c7db44e5cab3564d968184e55ecd7d
d-putnam <dan@dputnam.net>
2022-11-18 18:08:39 -0500

    Updates

--- precache-manifest.a4cb7bf053916112861dddc19b1d77d8.js
+++ precache-manifest.a4cb7bf053916112861dddc19b1d77d8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "b24ac675a6a6b4036e73",
-    "url": "/d-putnam.github.io/static/css/main.48ceb531.chunk.css"
+    "revision": "d6e33db2790065297357",
+    "url": "/d-putnam.github.io/static/css/main.9120e4a0.chunk.css"
   },
   {
-    "revision": "b24ac675a6a6b4036e73",
-    "url": "/d-putnam.github.io/static/js/main.b24ac675.chunk.js"
+    "revision": "d6e33db2790065297357",
+    "url": "/d-putnam.github.io/static/js/main.d6e33db2.chunk.js"
   },
   {
-    "revision": "c0fb30b4d923deb503af",
+    "revision": "9e41025356da25475a93",
     "url": "/d-putnam.github.io/static/css/1.507b4c18.chunk.css"
   },
   {
-    "revision": "c0fb30b4d923deb503af",
-    "url": "/d-putnam.github.io/static/js/1.c0fb30b4.chunk.js"
+    "revision": "9e41025356da25475a93",
+    "url": "/d-putnam.github.io/static/js/1.9e410253.chunk.js"
   },
   {
     "revision": "f403a79d0db09c27c66c",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/d-putnam.github.io/static/media/pic.77c93710.jpg"
   },
   {
-    "revision": "de224e6904b959e43a33dfb6a1c76e85",
+    "revision": "c44402323c1c9321ad1218d1d68e4c09",
     "url": "/d-putnam.github.io/index.html"
   }
 ];
