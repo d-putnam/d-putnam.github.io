4f0c46d901cc3c73f787aec584bb132c039c675b
d-putnam <dan@dputnam.net>
2022-11-18 15:25:02 -0500

    Updates

---
+++ precache-manifest.c6cbd180c1b71557095947b3c65c085d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "06d41291cd13b77bfebe",
+    "url": "/d-putnam.github.io/static/css/main.f7811c5f.chunk.css"
+  },
+  {
+    "revision": "06d41291cd13b77bfebe",
+    "url": "/d-putnam.github.io/static/js/main.06d41291.chunk.js"
+  },
+  {
+    "revision": "69388c0b657519870f26",
+    "url": "/d-putnam.github.io/static/css/1.507b4c18.chunk.css"
+  },
+  {
+    "revision": "69388c0b657519870f26",
+    "url": "/d-putnam.github.io/static/js/1.69388c0b.chunk.js"
+  },
+  {
+    "revision": "f403a79d0db09c27c66c",
+    "url": "/d-putnam.github.io/static/js/runtime~main.f403a79d.js"
+  },
+  {
+    "revision": "77c937100bccf1aea8bac73ce26999c7",
+    "url": "/d-putnam.github.io/static/media/pic.77c93710.jpg"
+  },
+  {
+    "revision": "87d1865748721c6be1044f8e0b80576d",
+    "url": "/d-putnam.github.io/index.html"
+  }
+];
