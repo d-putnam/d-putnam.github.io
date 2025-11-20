bae1c8a710ce3300b7ef00f08b99daf340d31c4c
Dan Putnam <dan@dputnam.net>
2025-11-19 20:07:40 -0500

    fix mobile nav

--- src/Components/Nav/MobileNav.js
+++ src/Components/Nav/MobileNav.js
@@ -10,42 +10,37 @@ const MobileNav = () => {
     setNav(false)
   }
 
+  // Always render the panel, toggle open class for transitions
+  const navClass = showNav ? `${styles.MobileNav} ${styles.MobileNavOpen}` : styles.MobileNav;
+
   return (
-    <div className={showNav ? styles.MobileNavOpen : styles.MobileNav}
-          style={{width: showNav ? '100vw' : 'fit-content'}}>
-      {
-        location.pathname !== '/' &&
+    <div className={navClass}
+         style={{width: showNav ? '100vw' : 'fit-content'}}>
+      {location.pathname !== '/' && (
         <>
           <svg className={styles.mobileToggle} viewBox="0 0 512 512" height="2em" width="2em" onClick={() => setNav(!showNav)}>
             <path d='M 32 96 v 64 h 448 V 96 H 32 Z m 0 128 v 64 h 448 v -64 H 32 Z m 0 128 v 64 h 448 v -64 H 32 Z' />
-          </svg>     
-          {
-            showNav &&
-            <>
-              <Link to={'/code'} onClick={closeNav}>
-                <div className={styles.linkItem}>
-                  code
-                </div>
-              </Link>
-              <Link to={'/blog'} onClick={closeNav}>
-                <div className={styles.linkItem}>
-                  blog
-                </div>
-              </Link>
-              <Link to={'/about'} onClick={closeNav}>
-                <div className={styles.linkItem}>
-                  about
-                </div>
-              </Link>
-              <Link to={'/'} onClick={closeNav}>
-                <div className={styles.linkItem}>
-                  home
-                </div>
-              </Link>
-            </>
-          }
+          </svg>
+          <div className={styles.panelContent}>
+            {showNav && (
+              <>
+                <Link to={'/code'} onClick={closeNav}>
+                  <div className={styles.linkItem}>code</div>
+                </Link>
+                <Link to={'/blog'} onClick={closeNav}>
+                  <div className={styles.linkItem}>blog</div>
+                </Link>
+                <Link to={'/about'} onClick={closeNav}>
+                  <div className={styles.linkItem}>about</div>
+                </Link>
+                <Link to={'/'} onClick={closeNav}>
+                  <div className={styles.linkItem}>home</div>
+                </Link>
+              </>
+            )}
+          </div>
         </>
-      }
+      )}
     </div>
   );
 }
